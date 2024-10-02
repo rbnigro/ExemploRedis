8e0f9c2e0561d38201fec171814910f2b5fa1290
rbnigro <ronneynigro@gmail.com>
2024-10-02 18:05:00 -0300

    Implementando Gerenciador de Cache

--- src/main/java/com/ronney/cache/CacheApplication.java
+++ src/main/java/com/ronney/cache/CacheApplication.java
@@ -2,10 +2,12 @@ package com.ronney.cache;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 import java.sql.*;
 
 @SpringBootApplication
+@EnableCaching
 public class CacheApplication {
 
 	public static void main(String[] args) throws SQLException {
--- src/main/java/com/ronney/cache/service/EmpresaService.java
+++ src/main/java/com/ronney/cache/service/EmpresaService.java
@@ -3,6 +3,7 @@ package com.ronney.cache.service;
 import com.ronney.cache.entity.Empresa;
 import com.ronney.cache.repository.EmpresaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,9 +11,11 @@ import java.util.List;
 @Service
 public class EmpresaService {
 
+
     @Autowired
     private EmpresaRepository empresaRepository;
 
+    @Cacheable("empresas")
     public List<Empresa> finadAll() {
         return (List<Empresa>) empresaRepository.findAll();
     }
